824dedef31690e2b12a37c8c4886ca6b7fda22a8
Ryan Long <rlong@redhat.com>
2019-10-25 09:33:41 -0400

    Split pages to serve on different urls (#1)
    * split out pages
    
    * remove overview
    
    * updating for sub apps

--- src/PresentationalComponents/ProductList.js
+++ src/PresentationalComponents/ProductList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { Card, CardHeader, CardBody, Stack, StackItem, Title } from '@patternfly/react-core';
+import { Card, CardHeader, CardBody, Stack, StackItem } from '@patternfly/react-core';
 
 import './product-list.scss';
 
@@ -12,28 +12,25 @@ class ProductList extends Component {
         const { data } = this.props;
 
         return (
-            <Card>
-                <CardHeader> { data.title } { data.version } </CardHeader>
-                <CardBody>
-                    <Stack gutter='lg'>
-                        { Object.entries(data.sections).map(([ name, info ]) => (
-                            <StackItem key={ name } className='ins-c-product-doc__info'>
-                                <Title headingLevel='h6' size='md' className='ins-c-product-doc__info--name'> { name } </Title>
-                                <Stack gutter='md'>
-                                    { info.map((section) => (
-                                        <StackItem key={ section.title }>
-                                            <dl>
-                                                <dt><a href={ section.url }> { section.title } </a></dt>
-                                                { section.description && <dd> { section.description } </dd> }
-                                            </dl>
-                                        </StackItem>
-                                    )) }
-                                </Stack>
-                            </StackItem>
-                        )) }
-                    </Stack>
-                </CardBody>
-            </Card>
+            <React.Fragment>
+                { Object.entries(data.sections).map(([ name, info ]) => (
+                    <Card className='ins-c-product-documentation__card' key= { name }>
+                        <CardHeader className='ins-c-product-doc__info'> { name } </CardHeader>
+                        <CardBody>
+                            <Stack gutter='md'>
+                                { info.map((section) => (
+                                    <StackItem key={ section.title }>
+                                        <dl>
+                                            <dt><a href={ section.url }> { section.title } </a></dt>
+                                            { section.description && <dd> { section.description } </dd> }
+                                        </dl>
+                                    </StackItem>
+                                )) }
+                            </Stack>
+                        </CardBody>
+                    </Card>
+                )) }
+            </React.Fragment>
         );
     }
 }
